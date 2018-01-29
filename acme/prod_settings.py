37b97e224292737367691323471268008b616a65
Gabriel Brun <gbrun@altima-agency.com>
2018-01-29 19:15:28 +0100

    fix DATABASES with S

--- acme/prod_settings.py
+++ acme/prod_settings.py
@@ -3,7 +3,7 @@ from .settings import *
 DEBUG = False
 TEMPLATE_DEBUG = False
 
-DATABASE['default'] = dj_database_url.config()
+DATABASES['default'] = dj_database_url.config()
 
 
 SECRET_KEY = '2g5_c8vsy4r%_snk0u6z@lk)f##k_fu(p2)iqv=(@fsl5cqc!*'
