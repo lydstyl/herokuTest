0e6aab05707df79d51ca30b2a02d95b1a8f67e9a
Gabriel Brun <gbrun@altima-agency.com>
2018-01-29 19:13:43 +0100

    config Postrgresql

--- acme/prod_settings.py
+++ acme/prod_settings.py
@@ -1,6 +1,10 @@
+import dj_database_url
 from .settings import *
 DEBUG = False
 TEMPLATE_DEBUG = False
+
+DATABASE['default'] = dj_database_url.config()
+
 
 SECRET_KEY = '2g5_c8vsy4r%_snk0u6z@lk)f##k_fu(p2)iqv=(@fsl5cqc!*'
 
